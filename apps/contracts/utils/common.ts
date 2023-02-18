5234bddcb88db955cf98db073ed0b4a034fdf7d1
Noy <59097122+noyyyy@users.noreply.github.com>
2023-02-19 01:23:26 +0800

    ✨ feat: use pnpm

--- apps/contracts/utils/common.ts
+++
@@ -1,7 +0,0 @@
-import { network } from 'hardhat';
-
-export const advanceBlock = async (secondsElaspse: number) => {
-  await network.provider.send('evm_mine');
-  await network.provider.send('evm_increaseTime', [secondsElaspse]);
-  console.log('\n  >>>>  advance time ' + secondsElaspse / 86400 + ' days  >>>>\n');
-};
